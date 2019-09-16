af6c6cc5d3f0dbad48abd56861149aceea8dfc1c
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2019-09-16 22:35:30 +0200

    bool added to types

--- include/micro/utils/types.h
+++ include/micro/utils/types.h
@@ -2,6 +2,7 @@
 #define MICRO_UTILS_TYPES_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -12,9 +13,6 @@ extern "C" {
  * @param b The second value.
  **/
 #define CONCAT(a,b) a##b
-
-typedef float float32_t;    // 32-bit floating point number
-typedef double float64_t;   // 64-bit floating point number
 
 void onHardFault();
 
