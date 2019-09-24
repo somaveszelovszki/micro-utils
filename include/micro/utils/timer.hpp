be8a7203eb97a085e1a08b322f193e909148e11a
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2019-09-24 22:46:21 +0200

    Timer implementation added

---
+++ include/micro/utils/timer.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <micro/bsp/tim.hpp>
+#include <micro/container/vec.hpp>
+
+namespace micro {
+
+#define MAX_NUM_TIMERS 64
+
+class Timer {
+public:
+    Timer(void)
+        : timedOut_(false)
+        , isRunning_(false) {
+        Timer::instances.append(this);
+    }
+
+    void start(const millisecond_t& period) {
+        this->startTime_= micro::getTime();
+        this->period_ = period;
+        this->timedOut_ = false;
+        this->isRunning_ = true;
+    }
+
+    void stop(void) {
+        this->isRunning_ = false;
+    }
+
+    bool checkTimeout(void) {
+        bool timedOut = this->timedOut_;
+        this->timedOut_ = false;
+        return timedOut;
+    }
+
+    static void updateAll(void) {
+        for (Timer *t : Timer::instances) {
+            t->update();
+        }
+    }
+
+private:
+    static vec<Timer*, MAX_NUM_TIMERS> instances;
+
+    void update(void) {
+        if (this->isRunning_ && getTime() - this->startTime_ > this->period_) {
+            this->startTime_ = getTime();
+            this->timedOut_ = true;
+        }
+    }
+
+    millisecond_t startTime_;
+    millisecond_t period_;
+    bool timedOut_;
+    bool isRunning_;
+};
+
+} // namespace micro
---
+++ src/timer.cpp
@@ -0,0 +1,5 @@
+#include <micro/utils/timer.hpp>
+
+namespace micro {
+vec<Timer*, MAX_NUM_TIMERS> Timer::instances;
+} // namespace micro
