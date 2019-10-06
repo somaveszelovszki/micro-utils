37d4287268cfd53a0a3c5d9efdcc27c6a8cad502
Soma Veszelovszki <soma.veszelovszki@aimotive.com>
2019-10-06 21:58:23 +0200

    MPU9250 driver added, tested, linearized

---
+++ include/micro/hw/MPU9250_Gyroscope.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <micro/bsp/i2c.hpp>
+#include <micro/bsp/tim.hpp>
+#include <micro/utils/point3.hpp>
+
+#include "math.h"
+namespace micro {
+namespace hw {
+void testGyro();
+
+}
+}
+
