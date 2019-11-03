6cd011da5ef28c8cc754fbd357762e92659a991c
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2019-11-03 21:07:52 +0100

    Panel class refactored

--- include/micro/panel/Panel.hpp
+++ include/micro/panel/Panel.hpp
@@ -1,20 +1,33 @@
 #pragma once
 
 #include <micro/utils/time.hpp>
+#include <micro/utils/unit_utils.hpp>
 
 #include "stm32f4xx_hal.h"
 #include "stm32f4xx_hal_uart.h"
+
+#include <FreeRTOS.h>
+#include <task.h>
 
 namespace micro {
 
 template <typename T_toPanel, typename T_fromPanel>
 class Panel {
 public:
-    explicit Panel(UART_HandleTypeDef *huart) : huart(huart) {}
+    explicit Panel(UART_HandleTypeDef *huart)
+        : huart(huart)
+        , newValueReceived(false) {}
 
     void start(const T_toPanel& out) {
-        HAL_UART_Receive_DMA(this->huart, reinterpret_cast<uint8_t*>(&this->inData), sizeof(T_fromPanel));
+        HAL_UART_Receive_DMA(this->huart, reinterpret_cast<uint8_t*>(&this->inBuffer), sizeof(T_fromPanel));
         this->send(out);
+    }
+
+    void waitStart(void) {
+        millisecond_t lastRecvTime = millisecond_t(0);
+        do {
+            vTaskDelay(1);
+        } while(!this->newValueReceived);
     }
 
     void send(const T_toPanel& out) {
@@ -22,25 +35,26 @@ public:
     }
 
     void onDataReceived(void) {
-        this->lastRecvTime = micro::getTime();
+        this->inData = inBuffer;
+        this->newValueReceived = true;
     }
 
-    T_fromPanel getLastValue(millisecond_t * const lastRecvTime = nullptr) const {
-        if (lastRecvTime != nullptr)
-        {
-            *lastRecvTime = this->lastRecvTime;
-        }
-        return this->inData;
+    bool hasNewValue(void) const {
+        return this->newValueReceived;
     }
 
-    millisecond_t getLastRecvTime(void) const {
-        return this->lastRecvTime;
+    T_fromPanel acquireLastValue(void) {
+        taskENTER_CRITICAL();
+        const T_fromPanel data = this->inData;
+        this->newValueReceived = false;
+        taskEXIT_CRITICAL();
+        return data;
     }
 
 private:
     UART_HandleTypeDef *huart;
-    millisecond_t lastRecvTime;
-    T_fromPanel inData;
+    bool newValueReceived;
+    T_fromPanel inBuffer, inData;
 };
 
 }  // namespace micro
