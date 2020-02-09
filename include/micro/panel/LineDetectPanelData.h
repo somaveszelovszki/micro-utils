2970feecc075e3975438bc94d07bf090bfeaebdb
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-02-09 11:46:55 +0100

    line detect panel timeouts updated

--- include/micro/panel/LineDetectPanelData.h
+++ include/micro/panel/LineDetectPanelData.h
@@ -15,7 +15,7 @@ typedef struct {
 } __attribute__((packed)) lineDetectPanelDataIn_t;
 
 #define LINE_DETECT_PANEL_LINK_IN_PERIOD_MS   20u
-#define LINE_DETECT_PANEL_LINK_IN_TIMEOUT_MS  50u
+#define LINE_DETECT_PANEL_LINK_IN_TIMEOUT_MS  70u
     
 typedef struct {
     trackedLine_t values[MAX_NUM_LINES];
@@ -23,7 +23,7 @@ typedef struct {
 } __attribute__((packed)) lineDetectPanelDataOut_t;
 
 #define LINE_DETECT_PANEL_LINK_OUT_PERIOD_MS   0u
-#define LINE_DETECT_PANEL_LINK_OUT_TIMEOUT_MS  10u
+#define LINE_DETECT_PANEL_LINK_OUT_TIMEOUT_MS  20u
 
 #ifdef __cplusplus
 }
