343cb0e8f0931e69975ec603819e4fb7b83d7473
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-04-01 21:16:38 +0200

    timer updates start time at period check (no interrupt needed)

--- include/micro/utils/timer.hpp
+++ include/micro/utils/timer.hpp
@@ -5,15 +5,9 @@
 
 namespace micro {
 
-#define MAX_NUM_TIMERS 64
-
 class Timer {
 public:
-    Timer(void)
-        : timedOut_(false)
-        , isRunning_(false) {
-        Timer::instances.push_back(this);
-    }
+    Timer(void) : isRunning_(false) {}
 
     void setPeriod(const millisecond_t period) {
         this->period_ = period;
@@ -22,7 +16,6 @@ public:
     void start(const millisecond_t period) {
         this->startTime_= getTime();
         this->period_ = period;
-        this->timedOut_ = false;
         this->isRunning_ = true;
     }
 
@@ -31,30 +24,17 @@ public:
     }
 
     bool checkTimeout(void) {
-        bool timedOut = this->timedOut_;
-        this->timedOut_ = false;
-        return timedOut;
-    }
-
-    static void updateAll(void) {
-        for (Timer *t : Timer::instances) {
-            t->update();
+        bool hasTimedOut = false;
+        if (this->isRunning_ && getTime() - this->startTime_ > this->period_) {
+            this->startTime_ += this->period_;
+            hasTimedOut = true;
         }
+        return hasTimedOut;
     }
 
 private:
-    static vec<Timer*, MAX_NUM_TIMERS> instances;
-
-    void update(void) {
-        if (this->isRunning_ && getTime() - this->startTime_ > this->period_) {
-            this->startTime_ = getTime();
-            this->timedOut_ = true;
-        }
-    }
-
     millisecond_t startTime_;
     millisecond_t period_;
-    bool timedOut_;
     bool isRunning_;
 };
 
--- src/timer.cpp
+++
@@ -1,5 +0,0 @@
-#include <micro/utils/timer.hpp>
-
-namespace micro {
-vec<Timer*, MAX_NUM_TIMERS> Timer::instances;
-} // namespace micro
