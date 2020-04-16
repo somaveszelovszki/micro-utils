9eb546218c05087b6706679cae324ab3b8ebd86d
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-04-16 17:38:33 +0200

    state type added for timestamped values

---
+++ include/micro/utils/state.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <micro/utils/units.hpp>
+#include <micro/utils/time.hpp>
+
+namespace micro {
+
+template <typename T>
+class state_t {
+public:
+    state_t(const T& val)
+        : value_(val)
+        , timestamp_(getTime()) {}
+
+    state_t& operator=(const T& val) {
+        this->value_     = val;
+        this->timestamp_ = getTime();
+        return *this;
+    }
+
+    const T& value() const {
+        return this->value_;
+    }
+
+    millisecond_t timestamp() const {
+        return this->timestamp_;
+    }
+
+private:
+    T value_;
+    millisecond_t timestamp_;
+};
+
+} // namespace micro
