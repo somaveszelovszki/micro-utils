60d1d1393646d52e2d84200ceadc8105f9fa80ca
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-04-17 22:59:36 +0200

    DC motor implementation added

---
+++ include/micro/hw/DC_Motor.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <micro/utils/units.hpp>
+
+#if defined STM32F0
+#include <stm32f0xx_hal.h>
+#include <stm32f4xx_hal_tim.h>
+#elif defined STM32F4
+#include <stm32f4xx_hal.h>
+#include <stm32f4xx_hal_tim.h>
+#endif
+
+namespace micro {
+namespace hw {
+/* @brief Controls a DC motor using a timer configured in PWM mode.
+ **/
+class DC_Motor {
+
+public:
+    DC_Motor(TIM_HandleTypeDef *htim, const uint32_t chnl1, const uint32_t chnl2, const float maxDuty);
+
+    void write(float duty);
+
+private:
+    TIM_HandleTypeDef *htim_;
+    const uint32_t chnl1_;
+    const uint32_t chnl2_;
+    const float maxDuty_;
+};
+} // namespace hw
+} // namespace micro
---
+++ src/DC_Motor.cpp
@@ -0,0 +1,28 @@
+#include <micro/hw/DC_Motor.hpp>
+#include <micro/math/numeric.hpp>
+
+namespace micro {
+namespace hw {
+
+DC_Motor::DC_Motor(TIM_HandleTypeDef *htim, const uint32_t chnl1, const uint32_t chnl2, const float maxDuty)
+    : htim_(htim)
+    , chnl1_(chnl1)
+    , chnl2_(chnl2)
+    , maxDuty_(maxDuty) {
+
+    this->write(0.0f);
+}
+
+void DC_Motor::write(float duty) {
+
+    duty = clamp(duty, -this->maxDuty_, this->maxDuty_);
+
+    const uint32_t pwm1 = map<float, uint32_t>(duty, -1.0f, 1.0f, 0, static_cast<uint32_t>(this->htim_->Instance->ARR));
+    const uint32_t pwm2 = this->htim_->Instance->ARR - pwm1;
+
+    __HAL_TIM_SET_COMPARE(this->htim_, this->chnl1_, pwm1);
+    __HAL_TIM_SET_COMPARE(this->htim_, this->chnl2_, pwm2);
+}
+
+} // namespace hw
+} // namespace micro
