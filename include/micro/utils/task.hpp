008809832ea5aa319f49d88430d056faf46b4c25
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-04-30 18:02:28 +0200

    enter and axitCritical macros added for utils/task

--- include/micro/utils/task.hpp
+++ include/micro/utils/task.hpp
@@ -9,20 +9,26 @@
 
 #define os_taskSuspendAll() vTaskSuspendAll()
 #define os_taskResumeAll()  xTaskResumeAll()
+#define os_enterCritical()  taskENTER_CRITICAL()
+#define os_exitCritical()   taskEXIT_CRITICAL()
 #define os_delay(ms)        vTaskDelay(ms)
 
 #else
 
 #if defined STM32F0
 #include <stm32f0xx_hal.h>
-#include <stm32f0xx_hal_tim.h>
 #elif defined STM32F4
 #include <stm32f4xx_hal.h>
-#include <stm32f4xx_hal_tim.h>
 #endif
+
+#ifdef __GNUC__
+#include "cmsis_gcc.h"
+#endif // __GNUC__
 
 #define os_taskSuspendAll()
 #define os_taskResumeAll()
+#define os_enterCritical()  __disable_irq()
+#define os_exitCritical()   __enable_irq()
 #define os_delay(ms)        HAL_Delay(ms)
 
 #endif // OS_FREERTOS
