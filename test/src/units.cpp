c60c15113d79cf322a8d098c1cddc8fd48af7fe2
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-05-03 13:06:29 +0200

    units numeric limits test bug fixed

--- test/src/units.cpp
+++ test/src/units.cpp
@@ -7,5 +7,5 @@ using namespace micro;
 TEST(units, constructors) {
     EXPECT_EQ(0.0f, meter_t().get());
     EXPECT_EQ(10.0f, meter_t(10.0f).get());
-    EXPECT_EQ(std::numeric_limits<float>::infinity(), std::numeric_limits<radian_t>::infinity().get());
+    EXPECT_EQ(micro::numeric_limits<float>::infinity(), micro::numeric_limits<radian_t>::infinity().get());
 }
