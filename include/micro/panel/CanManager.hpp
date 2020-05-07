85fb381a369c6fb2300fc3bcb97edaf5cab19efd
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-05-07 21:31:30 +0200

    CanManager refactored, CanFrameHandler added

--- include/micro/panel/CanManager.hpp
+++ include/micro/panel/CanManager.hpp
@@ -3,43 +3,77 @@
 #if defined STM32F4
 
 #include <micro/container/map.hpp>
+#include <micro/port/task.hpp>
 #include <micro/utils/timer.hpp>
 
 #include "vehicleCanTypes.hpp"
+
+#include <stm32f4xx_hal.h>
+#include <stm32f4xx_hal_can.h>
 
 #include <functional>
 
 namespace micro {
 
-typedef std::function<void(const uint8_t * const)> canFrame_handler_fn_t;
+struct canFrame_t {
+    typedef uint16_t id_t;
+    union {
+        CAN_RxHeaderTypeDef rx;
+        CAN_TxHeaderTypeDef tx;
+    } header;
+    alignas(8) uint8_t data[8];
+};
 
 class CanManager {
 public:
+    typedef sorted_vec<uint16_t, 16> filters_t;
+    typedef uint8_t subscriberId_t;
+
     explicit CanManager(CAN_HandleTypeDef * const hcan, const uint32_t rxFifo, const millisecond_t rxTimeout);
 
-    void registerHandler(const uint16_t id, const canFrame_handler_fn_t& handler);
+    subscriberId_t registerSubscriber(const filters_t& rxFilters);
 
-    void handleIncomingFrames();
+    bool read(const subscriberId_t subscriberId, canFrame_t& frame);
 
     template <typename T>
     void send(const T& data) {
         CAN_TxHeaderTypeDef txHeader = can::buildHeader<T>();
         uint32_t txMailbox = 0;
-        HAL_CAN_AddTxMessage(hcan_, &txHeader, const_cast<uint8_t*>(reinterpret_cast<const uint8_t*>(&data)), &txMailbox);
+        HAL_CAN_AddTxMessage(this->hcan_, &txHeader, const_cast<uint8_t*>(reinterpret_cast<const uint8_t*>(&data)), &txMailbox);
     }
 
     bool hasRxTimedOut() const {
         return this->rxWatchdog_.hasTimedOut();
     }
 
-private:
-    void handleIncomingFrame(const uint32_t id, const uint8_t * const data);
+    void onFrameReceived();
 
-    typedef sorted_map<uint16_t, canFrame_handler_fn_t, 64> handler_map_t;
+private:
+    struct subscriber_t {
+        filters_t rxFilters_;
+        queue_t<canFrame_t, 16> rxFrames_;
+    };
+
     CAN_HandleTypeDef * const hcan_;
     const uint32_t rxFifo_;
-    handler_map_t handlers_;
     WatchdogTimer rxWatchdog_;
+    mutex_t registerMutex_;
+    vec<subscriber_t, 4> subscribers_;
+};
+
+class CanFrameHandler {
+public:
+    typedef std::function<void(const uint8_t * const)> handler_fn_t;
+
+    void registerHandler(const canFrame_t::id_t frameId, const handler_fn_t& handler);
+
+    void handleFrame(const canFrame_t& rxFrame);
+
+    CanManager::filters_t identifiers() const;
+
+private:
+    typedef sorted_map<canFrame_t::id_t, handler_fn_t, 16> handlers_t;
+    handlers_t handlers_;
 };
 
 }  // namespace micro
--- src/CanManager.cpp
+++ src/CanManager.cpp
@@ -1,34 +1,57 @@
 #include <micro/panel/CanManager.hpp>
+#include <micro/utils/algorithm.hpp>
 
 #if defined STM32F4
 
 namespace micro {
+
+void CanFrameHandler::registerHandler(const canFrame_t::id_t frameId, const handler_fn_t& handler) {
+    this->handlers_.put(frameId, handler);
+}
+
+void CanFrameHandler::handleFrame(const canFrame_t& rxFrame) {
+    handler_fn_t * const handler = this->handlers_.get(rxFrame.header.rx.StdId);
+    if (handler) {
+        (*handler)(rxFrame.data);
+    }
+}
+
+CanManager::filters_t CanFrameHandler::identifiers() const {
+    CanManager::filters_t ids;
+    for (const handlers_t::entry_type& entry : this->handlers_) {
+        ids.insert(entry.first);
+    }
+    return ids;
+}
 
 CanManager::CanManager(CAN_HandleTypeDef * const hcan, const uint32_t rxFifo, const millisecond_t rxTimeout)
     : hcan_(hcan)
     , rxFifo_(rxFifo)
     , rxWatchdog_(rxTimeout) {}
 
-void CanManager::registerHandler(const uint16_t id, const canFrame_handler_fn_t& handler) {
-    this->handlers_.put(id, handler);
+CanManager::subscriberId_t CanManager::registerSubscriber(const filters_t& rxFilters) {
+    lock_guard_t lock(this->registerMutex_);
+    subscriber_t sub;
+    sub.rxFilters_ = rxFilters;
+    this->subscribers_.push_back(sub);
+    return this->subscribers_.size() - 1;
 }
 
-void CanManager::handleIncomingFrames() {
-    CAN_RxHeaderTypeDef rxHeader;
-    alignas(8) uint8_t rxData[8];
+bool CanManager::read(const subscriberId_t subscriberId, canFrame_t& frame) {
+    return this->subscribers_[subscriberId].rxFrames_.receive(frame);
+}
 
-    while (HAL_CAN_GetRxFifoFillLevel(this->hcan_, this->rxFifo_)) {
-        if (HAL_OK == HAL_CAN_GetRxMessage(this->hcan_, this->rxFifo_, &rxHeader, rxData)) {
-            this->rxWatchdog_.reset();
-            this->handleIncomingFrame(rxHeader.StdId, rxData);
+void CanManager::onFrameReceived() {
+    canFrame_t rxFrame;
+    if (HAL_OK == HAL_CAN_GetRxMessage(this->hcan_, this->rxFifo_, &rxFrame.header.rx, rxFrame.data)) {
+        this->rxWatchdog_.reset();
+
+        for (subscriber_t& sub : this->subscribers_) {
+            filters_t::const_iterator filter = micro::find_sorted(sub.rxFilters_.begin(), sub.rxFilters_.end(), rxFrame.header.rx.StdId);
+            if (filter != sub.rxFilters_.end()) {
+                sub.rxFrames_.send(rxFrame);
+            }
         }
-    }
-}
-
-void CanManager::handleIncomingFrame(const uint32_t id, const uint8_t * const data) {
-    canFrame_handler_fn_t *handler = this->handlers_.get(id);
-    if (handler) {
-        (*handler)(data);
     }
 }
 
