d06c1567dbceb84425fbec6970545b7ebadd5881
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-05-09 15:46:42 +0200

    utest added for CarProps::getSpeedAngle()

---
+++ test/src/utest_carprops.cpp
@@ -0,0 +1,33 @@
+#include <micro/utils/CarProps.hpp>
+
+#include "utils.hpp"
+
+using namespace micro;
+
+namespace {
+
+constexpr meter_t CAR_FRONT_REAR_PIVOT_DIST = centimeter_t(30);
+
+} // namespace
+
+TEST(CarProps, getSpeedAngle) {
+    CarProps car;
+
+    for (car.pose.angle = radian_t(0); car.pose.angle < 2 * PI; car.pose.angle += degree_t(20)) {
+        car.frontWheelAngle = radian_t(0);
+        car.rearWheelAngle  = radian_t(0);
+        EXPECT_NEAR_UNIT(car.pose.angle, car.getSpeedAngle(CAR_FRONT_REAR_PIVOT_DIST), degree_t(1));
+
+        car.frontWheelAngle = degree_t(15);
+        car.rearWheelAngle  = degree_t(15);
+        EXPECT_NEAR_UNIT(car.pose.angle + degree_t(15), car.getSpeedAngle(CAR_FRONT_REAR_PIVOT_DIST), degree_t(1));
+
+        car.frontWheelAngle = degree_t(-15);
+        car.rearWheelAngle  = degree_t(-15);
+        EXPECT_NEAR_UNIT(car.pose.angle - degree_t(15), car.getSpeedAngle(CAR_FRONT_REAR_PIVOT_DIST), degree_t(1));
+
+        car.frontWheelAngle = degree_t(15);
+        car.rearWheelAngle  = degree_t(-15);
+        EXPECT_NEAR_UNIT(car.pose.angle, car.getSpeedAngle(CAR_FRONT_REAR_PIVOT_DIST), degree_t(1));
+    }
+}
