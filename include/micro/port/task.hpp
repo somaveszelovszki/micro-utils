b85e84d32dd3c104f834c6e0c8506d09e3c0981d
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-10-13 21:32:56 +0200

    criticalSection_t added

--- include/micro/port/task.hpp
+++ include/micro/port/task.hpp
@@ -16,4 +16,20 @@ interruptStatus_t os_enterCritical();
 void os_exitCritical(const interruptStatus_t savedInterruptStatus);
 void os_sleep(const millisecond_t delay);
 
+class criticalSection_t {
+public:
+    bool lock() {
+        this->interruptStatus_ = os_enterCritical();
+        return true;
+    }
+
+    bool unlock() {
+        os_exitCritical(this->interruptStatus_);
+        return true;
+    }
+
+private:
+    uint32_t interruptStatus_;
+};
+
 } // namespace micro
