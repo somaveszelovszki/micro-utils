d7866f7a4e4fb53a53a2fa9095779488359365dd
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-11-14 23:05:01 +0100

    state_t now stores previous state to detect if value changed

--- include/micro/utils/state.hpp
+++ include/micro/utils/state.hpp
@@ -12,27 +12,40 @@ public:
 
     state_t(const T& value, const millisecond_t timestamp)
         : value_(value)
+        , prevValue_(value)
         , timestamp_(timestamp) {}
 
     state_t(const T& value) : state_t(value, getTime()) {}
 
     state_t(const state_t&) = default;
 
-    state_t& operator=(const T& val) {
-        this->value_     = val;
-        this->timestamp_ = getTime();
+    state_t& operator=(const T& value) {
+        this->set(value, getTime());
         return *this;
     }
 
     state_t& operator=(const state_t&) = default;
 
+    operator T() const {
+        return this->value_;
+    }
+
+    bool changed() const {
+        return this->value_ != this->prevValue_;
+    }
+
     void set(const T& value, const millisecond_t timestamp) {
-        this->value_ = value;
+        this->prevValue_ = this->value_;
+        this->value_     = value;
         this->timestamp_ = timestamp;
     }
 
     const T& value() const {
         return this->value_;
+    }
+
+    const T& prev() const {
+        return this->prevValue_;
     }
 
     millisecond_t timestamp() const {
@@ -41,6 +54,7 @@ public:
 
 private:
     T value_;
+    T prevValue_;
     millisecond_t timestamp_;
 };
 
