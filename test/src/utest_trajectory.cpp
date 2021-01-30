ba69461a58b3161b7ef4b416b4b33b2ae2eb6871
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2021-01-30 21:41:09 +0100

    unit test for multiple segment straight trajectory added

--- test/src/utest_trajectory.cpp
+++ test/src/utest_trajectory.cpp
@@ -114,6 +114,65 @@ void test_sine_arc_fix_orientation_perfect_follow(const Sign speedSign) {
     }
 }
 
+void test_straight_line_multiple_segments_perfect_follow(const Sign speedSign) {
+    Trajectory trajectory;
+
+    trajectory.setStartConfig(Trajectory::config_t{
+        Pose{ point2m{ meter_t(0), meter_t(0) }, radian_t(0) },
+        speedSign * m_per_sec_t(1.0f)
+    }, meter_t(0));
+
+    trajectory.appendLine(Trajectory::config_t{
+        Pose{
+            trajectory.lastConfig().pose.pos + vec2m{ meter_t(1), meter_t(0) },
+            radian_t(0)
+        },
+        speedSign * m_per_sec_t(1.5f)
+    });
+
+    trajectory.appendLine(Trajectory::config_t{
+        Pose{
+            trajectory.lastConfig().pose.pos + vec2m{ meter_t(1), meter_t(0) },
+            radian_t(0)
+        },
+        speedSign * m_per_sec_t(3.0f)
+    });
+
+    trajectory.appendLine(Trajectory::config_t{
+        Pose{
+            trajectory.lastConfig().pose.pos + vec2m{ meter_t(5), meter_t(0) },
+            radian_t(0)
+        },
+        speedSign * m_per_sec_t(3.0f)
+    });
+
+    CarProps car;
+    ControlData controlData;
+
+    for (meter_t dist = { 0 }; dist < trajectory.length(); dist += centimeter_t(20)) {
+        car.pose = { point2m{ dist, meter_t(0) }, radian_t(0) };
+        car.distance = dist;
+        controlData = trajectory.update(car);
+
+        m_per_sec_t expectedTargetSpeed;
+        if (dist < meter_t(1)) {
+            expectedTargetSpeed = map(dist, meter_t(0), meter_t(1), m_per_sec_t(1.0f), m_per_sec_t(1.5f));
+        } else if (dist < meter_t(2)) {
+            expectedTargetSpeed = map(dist, meter_t(1), meter_t(2), m_per_sec_t(1.5f), m_per_sec_t(3.0f));
+        } else {
+            expectedTargetSpeed = m_per_sec_t(3.0f);
+        }
+
+        EXPECT_NEAR_UNIT(speedSign * expectedTargetSpeed, controlData.speed, m_per_sec_t(0.1f));
+        EXPECT_EQ(millisecond_t(0), controlData.rampTime);
+        EXPECT_TRUE(controlData.rearSteerEnabled);
+        EXPECT_NEAR_UNIT(millimeter_t(0), controlData.lineControl.actual.pos, millimeter_t(1));
+        EXPECT_NEAR_UNIT(radian_t(0), controlData.lineControl.actual.angle, degree_t(1));
+        EXPECT_EQ(millimeter_t(0), controlData.lineControl.target.pos);
+        EXPECT_EQ(radian_t(0), controlData.lineControl.target.angle);
+    }
+}
+
 TEST(trajectory, straight_line_perfect_follow) {
     test_straight_line_perfect_follow(Sign::POSITIVE);
     test_straight_line_perfect_follow(Sign::NEGATIVE);
@@ -127,4 +186,9 @@ TEST(trajectory, straight_line_nonperfect_follow) {
 TEST(trajectory, sine_arc_fix_orientation_perfect_follow) {
     test_sine_arc_fix_orientation_perfect_follow(Sign::POSITIVE);
     test_sine_arc_fix_orientation_perfect_follow(Sign::NEGATIVE);
+}
+
+TEST(trajectory, straight_line_multiple_segments_perfect_follow) {
+    test_straight_line_multiple_segments_perfect_follow(Sign::POSITIVE);
+    test_straight_line_multiple_segments_perfect_follow(Sign::NEGATIVE);
 }
