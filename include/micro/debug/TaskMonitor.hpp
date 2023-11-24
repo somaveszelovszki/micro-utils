acebd34159d008ba4c0dce6f1cc6d698c0f8f1af
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2023-11-24 20:54:40 +0100

    TaskManager bugs fixed, storing task name

--- include/micro/debug/TaskMonitor.hpp
+++ include/micro/debug/TaskMonitor.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <etl/string.h>
+
 #include <micro/container/map.hpp>
+#include <micro/port/mutex.hpp>
 #include <micro/port/sync_barrier.hpp>
 #include <micro/port/task.hpp>
 #include <micro/utils/state.hpp>
@@ -18,8 +21,14 @@ public:
     bool ok() const;
 
 private:
+    struct State {
+        etl::string<64> name;
+        state_t<bool> ok;
+    };
+
+    mutex_t registerMutex_;
     sync_barrier_t initializationBarrier_;
-    micro::map<taskId_t, state_t<bool>, 16> taskStates_;
+    micro::map<taskId_t, State, 16> taskStates_;
 };
 
 } // namespace micro
--- src/TaskMonitor.cpp
+++ src/TaskMonitor.cpp
@@ -1,6 +1,7 @@
 #include <micro/debug/TaskMonitor.hpp>
 
 #include <algorithm>
+#include <mutex>
 
 #include <micro/log/log.hpp>
 #include <micro/port/task.hpp>
@@ -8,20 +9,25 @@
 namespace micro {
 
 void TaskMonitor::registerInitializedTask() {
-    const auto event = (1 << taskStates_.size());
-    taskStates_.insert(std::make_pair(getCurrentTaskId(), false));
+    const auto event = [this](){
+        const auto info = getCurrentTaskInfo();
+        std::scoped_lock lock{registerMutex_};
+        taskStates_.insert(std::make_pair(info.id, State{info.name, false}));
+        return (1 << (taskStates_.size() - 1));
+    }();
+
     initializationBarrier_.sync(event);
 }
 
 void TaskMonitor::notify(const bool state) {
     if (const auto it = taskStates_.find(getCurrentTaskId()); it != taskStates_.end()) {
-        it->second = state;
+        it->second.ok = state;
     }
 }
 
 bool TaskMonitor::ok() const {
     return std::all_of(taskStates_.begin(), taskStates_.end(), [now = getTime()](const auto& entry){
-        const auto& state = entry.second;
+        const auto& state = entry.second.ok;
         return now - state.timestamp() < millisecond_t(50) && state.value();
     });
 }
