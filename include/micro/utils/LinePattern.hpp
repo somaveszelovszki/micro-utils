d4cdefe4d1873670546319853c2ea6e8ea0fa99c
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2024-02-03 19:11:01 +0100

    JUNCTION_CENTER line pattern added

--- include/micro/utils/LinePattern.hpp
+++ include/micro/utils/LinePattern.hpp
@@ -11,14 +11,15 @@ public:
     /* @brief Defines line pattern type.
      **/
     enum type_t : uint8_t {
-        NONE        = 0, ///< No lines detected
-        SINGLE_LINE = 1, ///< 1 solid 1.9mm wide line.
-        ACCELERATE  = 2, ///< 1 solid line in the middle, 2 dashed lines on both sides
-        BRAKE       = 3, ///< 1 solid line in the middle, 2 solid lines on both sides
-        LANE_CHANGE = 4, ///< 1 solid line in the middle, 1 dashed line on one (UNKNOWN!) side (dash and space lengths decreasing)
-        JUNCTION_1  = 5, ///< Labyrinth junction of 1 segment
-        JUNCTION_2  = 6, ///< Labyrinth junction of 2 segments
-        JUNCTION_3  = 7  ///< Labyrinth junction of 3 segments
+        NONE            = 0, ///< No lines detected
+        SINGLE_LINE     = 1, ///< 1 solid 1.9mm wide line.
+        ACCELERATE      = 2, ///< 1 solid line in the middle, 2 dashed lines on both sides
+        BRAKE           = 3, ///< 1 solid line in the middle, 2 solid lines on both sides
+        LANE_CHANGE     = 4, ///< 1 solid line in the middle, 1 dashed line on one (UNKNOWN!) side (dash and space lengths decreasing)
+        JUNCTION_1      = 5, ///< Labyrinth junction of 1 segment
+        JUNCTION_2      = 6, ///< Labyrinth junction of 2 segments
+        JUNCTION_3      = 7, ///< Labyrinth junction of 3 segments
+        JUNCTION_CENTER = 8 ///< Single line or 4 solid lines
     };
 
     type_t type    = type_t::NONE;        // Line pattern type.
